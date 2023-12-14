33ffb5432a45796517f2840479f4f92515061656
RYKT <rykt1024@163.com>
2023-12-14 20:20:24 +0800

    Change button to interrupt mode, and optimize the project structure

--- src/button.cpp
+++ src/button.cpp
@@ -5,54 +5,69 @@
 int button_1 = 22;
 int button_2 = 23;
 
-int click_status_1 = 0;
-int click_sum_1 = 0;
+volatile bool button_1_flag = false;
+volatile int button_1_clicksum = 0;
+volatile unsigned long button_1_lasttime = 0;
 
-int click_status_2 = 0;
-int click_sum_2 = 0;
+volatile bool button_2_flag = false;
+volatile int button_2_clicksum = 0;
+volatile unsigned long button_2_lasttime = 0;
+
+void button_1_handler();
+void button_2_handler();
 
 void setup_button() {
   // 初始化引脚模式
   pinMode(button_1, INPUT_PULLUP);
   pinMode(button_2, INPUT_PULLUP);
 
+  attachInterrupt(digitalPinToInterrupt(button_1), button_1_handler, FALLING);
+  attachInterrupt(digitalPinToInterrupt(button_2), button_2_handler, FALLING);
+
   // 初始化串口通信
-  Serial.begin(115200);
+  // Serial.begin(115200);
   
-  Serial.println("你好！");
+  button_1_lasttime = 0;
 }
 
 void button_1_handler() {
-  click_sum_1 ++;
-  Serial.print("button_1 is clicked. x");
-  Serial.println(click_sum_1);
+  bool status = digitalRead(button_1);
+  unsigned long current = millis();
+
+  if(!status) {
+    if ((current - button_1_lasttime) > 250) {
+      button_1_clicksum ++;
+      button_1_flag = true;
+      button_1_lasttime = current;
+    }
+  }
 }
 
 void button_2_handler() {
-  click_sum_2 ++;
-  led_switch();
-  Serial.print("button_2 is clicked. x");
-  Serial.println(click_sum_2);
+  bool status = digitalRead(button_2);
+  unsigned long current = millis();
+
+  if(!status) {
+    if ((current - button_2_lasttime) > 250) {
+      button_2_clicksum ++;
+      button_2_flag = true;
+      led_switch();
+      button_2_lasttime = current;
+    }
+  }
 }
 
 void loop_button() {
-  // 按钮消抖
-  if (!digitalRead(button_1)) {
-    click_status_2 = 0;
-    delay(10);
-    if (!digitalRead(button_1) && !click_status_1) { 
-      button_1_handler();
-      click_status_1 = 1;
-    }
-  } else if (!digitalRead(button_2)) {
-    click_status_1 = 0;
-    delay(10);
-    if (!digitalRead(button_2) && !click_status_2) { 
-      button_2_handler();
-      click_status_2 = 1;
-    }
-  } else {
-    click_status_1 = 0;
-    click_status_2 = 0;
-  }
+  // if(button_1_flag) {
+  //   // Serial.print("button 1 clicked.");
+  //   // Serial.print('x');
+  //   // Serial.println(button_1_clicksum);
+  //   button_1_flag = false;
+  // }
+  // else if(button_2_flag) {
+  //   // Serial.print("button 2 clicked.");
+  //   // Serial.print('x');
+  //   // Serial.println(button_2_clicksum);
+  //   button_2_flag = false;
+  // }
 } 
